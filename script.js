36c8df90c4c3ec8b3ef9d1c3e3e08b43a3112882
tum150 <90237393+tum150@users.noreply.github.com>
2023-11-24 15:19:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+﻿// Initialize LIFF
+liff.init({
+    liffId: '2001803237-GPaWyDag'
+}).then(() => {
+    // Check if the user is logged in
+    if (!liff.isLoggedIn()) {
+        // Open an external browser for LINE Login
+        liff.openWindow({
+            url: 'rt.penagroup.com',
+            external: true
+        });
+    } else {
+        // User is already logged in, proceed with your app logic
+    }
+}).catch((err) => {
+    console.error('LIFF initialization failed', err);
+});
+
+// Handle the login callback after the user logs in through LINE Login
+function handleLoginCallback() {
+    // Retrieve the access token and make requests on behalf of the user
+    const accessToken = liff.getAccessToken();
+    // Your logic here...
+}
